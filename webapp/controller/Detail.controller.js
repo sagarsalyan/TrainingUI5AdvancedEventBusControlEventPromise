5d567af3dc038b05681c0daa3dda3b918805299a
Sagar Salyan <sagarslyn@gmail.com>
2022-07-03 08:00:18 +0000

    added browser event attach function

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -13,23 +13,23 @@ sap.ui.define([
 		onInit: function () {
 
 		},
-		onSubmit:function(){
+		onSubmit: function () {
 			var empModel = this.getView().getModel("empModel");
 			var oEventBus = sap.ui.getCore().getEventBus();
 			var oMessage = {
-				empData:{},
-				message:"Data saved"
+				empData: {},
+				message: "Data saved"
 			};
 			var empData = empModel.getProperty("/tempData");
 			empData.id = empModel.getProperty("/results").length + 1;
 			empModel.getProperty("/results").push(empData);
 			empModel.refresh();
 			oMessage.empData = empData;
-			
+
 			//firing event bus
-			oEventBus.publish("NotifyChannel","showMessageBox",oMessage);
-			
-		}
+			oEventBus.publish("NotifyChannel", "showMessageBox", oMessage);
+
+		},
 
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
@@ -45,9 +45,25 @@ sap.ui.define([
 		 * This hook is the same one that SAPUI5 controls get after being rendered.
 		 * @memberOf app.UI5AdvancedTraining.view.Detail
 		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
+		onAfterRendering: function () {
+			var inputName = this.getView().byId("name");
+			// way 1
+			inputName.attachBrowserEvent("click", function () {
+			//code goes here
+				debugger;
+			}.bind(this));
+
+			// way 2
+			// inputName.addEventDelegate({
+			// 	onclick: function(e) {
+			// 		//code goes here
+			// 		debugger;
+			// 	}.bind(this),
+			// 	onkeydown: function(e) {
+			// 		e.preventDefault();
+			// 	}.bind(this),
+			// });
+		},
 
 		/**
 		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
